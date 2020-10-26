633361c30035a5cf838b0444c3fa02322574696e
cuixiaoxu <cuixiaoxu@9fbank.com.cn>
2020-10-26 09:04:24 +0800

    表结构

--- src/main/java/com/example/demo/controller/StoreGoodsRecordController.java
+++ src/main/java/com/example/demo/controller/StoreGoodsRecordController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -76,7 +77,7 @@ public class StoreGoodsRecordController {
     /**
      * 修改
      */
-    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    @PutMapping(value = "/update")
     public int updateStoreGoodsRecord( @RequestBody @Valid StoreGoodsRecordVo storeGoodsRecordVo){
         return storeGoodsRecordService.updateStoreGoodsRecord(storeGoodsRecordVo);
     }
