550124d68223b6f919cdc7673dced3835b319668
cuixiaoxu <xiaoxu.cui@opay-inc.com>
2023-05-26 14:30:22 +0800

    test

--- src/test/java/com/example/demo/Test3.java
+++ src/test/java/com/example/demo/Test3.java
@@ -7,9 +7,11 @@ import com.example.demo.service.impl.StoreGoodsRecordServiceImpl;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Test3 {
 
@@ -31,8 +33,16 @@ public class Test3 {
 //        ArrayList<Map<String, Object>> list = JSON.parseObject("[{\"name\":\"msg\",\"comment\":\"消息\",\"type\":\"long\"},{\"name\":\"msg\",\"comment\":\"消息\",\"type\":\"long\"}]", ArrayList.class);
 //        System.out.println(list);
 //        // Object value = cache.get(id, t -> setValue(id).apply(id));
-        String a = "10.02";
-        long l = new BigDecimal(a).setScale(0, RoundingMode.UP).longValue();
-        System.out.println(l);
+//        String a = "10.02";
+//        long l = new BigDecimal(a).setScale(0, RoundingMode.UP).longValue();
+//        System.out.println(l);
+//        int i = 0;
+//        int a = ++i;
+//        System.out.println("a的值是:"+a);
+//        System.out.println("i的值是:"+i);
+//        for (int i = 0; i < 3; i++) {
+//            System.out.println("i的值是:"+i);
+//        }
+        System.out.println(Arrays.stream("56".split("\\|")).map(Integer::valueOf).collect(Collectors.toList()));
     }
 }
